96dce511fbdbe64091e888b8f9f053a97c72b5a5
s4m.n0v <s4m.n0v@gmail.com>
2025-01-14 21:52:45 +0000

    New desing for login

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="max-w-md w-full space-y-8 bg-white p-8 rounded-xl shadow-md">
-      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-        {{ isRegistering ? 'Register' : 'Sign in to your account' }}
+  <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <div class="max-w-md w-full space-y-8 bg-white dark:bg-gray-800 p-8 rounded-xl shadow-md">
+      <div class="flex justify-end">
+        <Theme />
+      </div>
+      <h2 class="text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+        {{ isRegistering ? 'Registrarse' : 'Ingresar a Tutor IUDC' }}
       </h2>
       <form class="mt-8 space-y-6" @submit.prevent="handleSubmit">
         <input type="hidden" name="remember" value="true" />
@@ -10,46 +13,51 @@
           <div>
             <label for="email-address" class="sr-only">Email address</label>
             <input id="email-address" name="email" type="email" autocomplete="email" required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-white dark:bg-gray-700"
               placeholder="Email address" v-model="email" />
           </div>
 
           <div v-if="isRegistering">
             <label for="documentoIdentidad" class="sr-only">Documento de Identidad</label>
             <input id="documentoIdentidad" name="documentoIdentidad" type="text" required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-white dark:bg-gray-700"
               placeholder="Documento de Identidad" v-model="documentoIdentidad" />
           </div>
 
           <div v-if="isRegistering">
             <label for="nombre" class="sr-only">Nombre</label>
             <input id="nombre" name="nombre" type="text" required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-white dark:bg-gray-700"
               placeholder="Nombre" v-model="nombre" />
           </div>
 
           <div v-if="isRegistering">
             <label for="telefono" class="sr-only">Teléfono</label>
             <input id="telefono" name="telefono" type="tel" required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-white dark:bg-gray-700"
               placeholder="Teléfono" v-model="telefono" />
           </div>
 
           <div v-if="isRegistering">
-            <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
+            <label for="role" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Role</label>
             <select id="role" name="role"
-              class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+              class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
               v-model="role">
               <option value="ADMIN">Admin</option>
               <option value="DOCENTE">Teacher</option>
             </select>
           </div>
-
-          <div>
+          <div class="relative">
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required
-              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            <input :type="showPassword ? 'text' : 'password'" id="password" name="password"
+              autocomplete="current-password" required
+              class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm bg-white dark:bg-gray-700"
               placeholder="Password" v-model="password" />
+            <button type="button" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+              @click="showPassword = !showPassword">
+              <Icon :name="showPassword ? 'heroicons:eye-slash' : 'i-heroicons-eye'"
+                class="h-5 w-5 text-black dark:text-white" />
+            </button>
           </div>
         </div>
 
@@ -59,15 +67,16 @@
 
         <div>
           <button type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
             {{ isRegistering ? 'Register' : 'Sign in' }}
           </button>
         </div>
       </form>
       <div class="text-center">
-        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"
+        <a href="#"
+          class="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300"
           @click.prevent="isRegistering = !isRegistering">
-          {{ isRegistering ? 'Already have an account? Sign in' : 'Need an account? Register' }}
+          {{ isRegistering ? 'Ya tienes cuenta? Ingresa' : 'Necesitas cuenta? Registrate' }}
         </a>
       </div>
     </div>
@@ -84,10 +93,11 @@ const route = useRoute()
 const { login, register, isAuthenticated } = useAuth()
 const email = ref('')
 const password = ref('')
-const role = ref('ESTUDIANTE')
+const role = ref('ADMIN')
 const documentoIdentidad = ref('')
 const nombre = ref('')
 const telefono = ref('')
+const showPassword = ref(false);
 
 // Set initial state based on query parameter
 const isRegistering = ref(route.query.register === 'true')
